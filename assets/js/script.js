24b879918a707978555dee93789eca1795d87cb2
MarcoGotti <mrc.gotti@gmail.com>
2023-12-02 22:37:23 +0000

    fix 5th round js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 //MAIL
 
-console.log('ESERCIZIO  MAIL');
-
 //lista email che hanno accesso
-const list =['mrc.gotti@gmail.com', 'abc@virgilio.it', '123@libero.it']
+const list =['1@gmail.com', '2@virgilio.it', '3@libero.it']
 
 //chiedi a user la sua email
 let userEmail = prompt('Insert your email');
+console.log('ESERCIZIO  MAIL');
 let correctEmail = false;
 console.log(correctEmail);
 
 //confronta
 for (let i = 0; i < list.length; i++) {
     let element = list[i]; 
-    if (element = userEmail){
+    if (element == userEmail){  // perchè == ?
         correctEmail = true;
         console.log(`Good! You are in
 ${userEmail} is a valid email`);
@@ -33,15 +32,12 @@ console.log('ESERCIZIO  DADI');
 
 //genera numero random 1-6
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min +1)) + min;
-  }
-const firstDice = Number(getRndInteger(1, 6))
+const firstDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
 console.log('Player One:', firstDice);
 
 //genera altro numero 1-6
 
-const secondDice = Number(getRndInteger(1, 6))
+const secondDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
 console.log('Player Two:', secondDice);
 
 
