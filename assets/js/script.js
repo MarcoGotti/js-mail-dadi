aab72e20f1d5c0742706ce96d479c9a5c806d274
MarcoGotti <mrc.gotti@gmail.com>
2023-12-02 19:43:44 +0000

    fix 3rd round js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,22 @@ const list =['mrc.gotti@gmail.com', 'abc@virgilio.it', '123@libero.it']
 
 //chiedi a user la sua email
 let userEmail = prompt('Insert your email');
-console.log(userEmail === list[0]);
+let correctEmail = false;
+console.log(correctEmail);
 
 //confronta
 for (let i = 0; i < list.length; i++) {
-    var element = list[i];
-    console.log(element === userEmail); 
-}
- 
-if (element === userEmail){
-    console.log('Good! You are in');
+    let element = list[i]; 
+    if (element === userEmail){
+        correctEmail = true;
+        console.log('Good! You are in');
+   }
+
 }
 
+if (correctEmail == false){
+    console.log('No, you are out')
+}
 
 
 //GIOCO DEI DADI
