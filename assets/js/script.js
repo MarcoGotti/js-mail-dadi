abb5f9be29453d9a72f096c4b3a9151ebf56fe68
MarcoGotti <mrc.gotti@gmail.com>
2023-12-05 01:33:14 +0000

    head commit
    add markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,72 @@
 //lista email che hanno accesso
 const list =['1@gmail.com', '2@virgilio.it', '3@libero.it']
 
-//chiedi a user la sua email
-let userEmail = prompt('Insert your email');
-console.log('ESERCIZIO  MAIL');
-let correctEmail = false;
-console.log(correctEmail);
+//check by clicking
+const buttonEmail = document.getElementById('buttonEmail');
+buttonEmail.addEventListener('click', function() {
+    //read input
+    const userEmail = document.getElementById('emailInput').value
+    let correctEmail = false;
+    for (let i = 0; i < list.length; i++) {
+        let element = list[i]; 
+        if (element === userEmail){  
+            correctEmail = true;
+            document.getElementById('positiveCheck').classList.remove('d-none');
+            document.getElementById('emailInput').classList.remove('is-invalid'); 
+       }
+    
+    } 
+    if (correctEmail === false){  
+        document.getElementById('positiveCheck').classList.add('d-none');
+        document.getElementById('emailInput').classList.add('is-invalid');  
+    }
+    
+});
 
-//confronta
-for (let i = 0; i < list.length; i++) {
-    let element = list[i]; 
-    if (element == userEmail){  // perchè == ?
-        correctEmail = true;
-        console.log(`Good! You are in
-${userEmail} is a valid email`);
-   }
+//clean up the input
+emailInput.addEventListener('click', function(){ //***why do I need two clicks????
+    document.getElementById('emailInput').classList.remove('is-invalid'); 
+});
 
-}
 
-if (correctEmail == false){   //perchè ==  ?
-    console.log(`No way!
-${userEmail} is not a valid`)
-}
 
 
 //GIOCO DEI DADI
 
-console.log('ESERCIZIO  DADI');
+//click and play
+const buttonDice  = document.getElementById('buttonDice');
+buttonDice.addEventListener('click', function() { //***perchè se refresho non tornano i value iniziali??
+    
+    //genera numero random 1-6
 
-//genera numero random 1-6
+    const firstDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
+    document.getElementById('userDice').value = firstDice;
 
-const firstDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
-console.log('Player One:', firstDice);
+    //genera altro numero 1-6
 
-//genera altro numero 1-6
+    const secondDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
+    document.getElementById('bobbyDice').value = secondDice;
 
-const secondDice = Math.floor(Math.random() * (6 - 1 +1)) + 1;
-console.log('Player Two:', secondDice);
+    //genera messaggio esito 
+        //create const utility
+        const userDice = document.getElementById('userDice');
+        const bobbyDice = document.getElementById('bobbyDice');
+    if (firstDice > secondDice){
+        userDice.classList.add('border', 'border-danger', 'border-3');
+        bobbyDice.classList.remove('border', 'border-danger', 'border-success', 'border-2');
+        document.querySelector('span').innerText = ('You WIN !!!')
+    } else if (secondDice > firstDice) {
+        bobbyDice.classList.add('border', 'border-danger', 'border-3');
+        userDice.classList.remove('border', 'border-danger', 'border-success', 'border-2');
+        document.querySelector('span').innerText = ('Bobby wins!')
+    } else{
+        document.querySelector('span').innerText = ('It\'s an even!')
+        document.getElementById('userDice', 'bobbyDice').classList.remove('border-danger');
+        bobbyDice.classList.add('border', 'border-success', 'border-3');
+        userDice.classList.add('border', 'border-success', 'border-3');
+    }
+})
 
-
-//genera messaggio esito 
-
-if (firstDice > secondDice){
-    console.log('The winner is Player One');
-} else if (secondDice > firstDice) {
-    console.log('The winner is Player Two');
-} else{
-    console.log('That\'s an even !!');
-}
 
 
 
